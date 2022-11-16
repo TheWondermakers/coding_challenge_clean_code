bce90e78c1ffffd2a860129fdc974dae876398d2
samirahuber <mail@samira-huber.de>
2022-11-16 19:57:20 +0100

    add python task

---
+++ python/task-1.py
@@ -0,0 +1,5 @@
+# Using Python, your assignment is to write a program that returns a list that contains only the elements that are common between two lists.
+a = [1, 1, 2, 3, 5, 9, 13, 14, 34, 55, 89]
+b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 15, 22, 54]
+
+# Make sure to create a list without duplicates and that your program works on both lists.
